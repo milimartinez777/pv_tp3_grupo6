6cd4b9428aad9177be9252524cb8a9f245f491d3
¨¨DavidJuarez¨ <davidrohomi@gmail.com>
2026-05-26 19:02:30 -0300

    Actualizacion archivo Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../css/footer.css";
+
+function Footer() {
+  return (
+    <footer className="piePagina" >
+      <p>@ Analista Programador Universitario - 2026 </p>
+    </footer>
+  );
+}
+
+export default Footer;
