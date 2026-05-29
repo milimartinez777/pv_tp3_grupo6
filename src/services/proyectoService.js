50844fd60053f773ddc2b65cea724a522f259b85
Jonatan Cruz <anibalc279@gmail.com>
2026-05-29 12:03:03 -0300

    Actualizar estructura de proyectoService para soportar datos nuevos y extendidos. Pasos de primera instancia para los nuevos requerimientos.

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,6 +1,20 @@
 const proyectoService = (( )=>{
     let proyectos = [
-        { id: 1, título: "Sistema de gestión de biblioteca escolar", categoría: "Programación", estado: "En progreso" },
+        { 
+        id: 1, 
+        título: "Sistema de gestión de biblioteca escolar", 
+        categoría: "Programación", 
+        estado: "En progreso",
+        descripcion: "Este proyecto surge de la necesidad de digitalizar los procesos administrativos y académicos de las instituciones educativas.\n\nPermite centralizar la información de estudiantes, docentes y tutores en una sola plataforma.",
+        recursos: {
+            pdf: "https://ejemplo.com/documento.pdf",
+            drive: "https://drive.google.com",
+            github: "https://github.com"
+        },
+        equipo: [
+            { nombre: "Juan Pérez", rol: "Desarrollador" }
+        ]
+        },
         { id: 2, título: "App de Matemática", categoría: "Matemática", estado: "Planificación" },
         { id: 3, título: "Campus virtual Educativo", categoría: "Diseño", estado: "Terminado" },
         { id: 4, título: "Organizador de tareas académicas", categoría: "Programación", estado: "En progreso" },
