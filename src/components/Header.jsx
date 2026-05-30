79664205eee754173b24c1623bdc6a4c20831b6a
Jonatan Cruz <anibalc279@gmail.com>
2026-05-30 20:33:25 -0300

    Agrego Logo en el header, con correccion respectivo del su css. Ademas agrego correcciones minimas de tipeo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import "../css/header.css";
 
+import logo from "../assets/logito.png";
 
 const Header = () =>{
   return (
     <header className="encabezado-sitio">
+      <img src={logo} alt="Logo del sitio" className="logo-sitio" />
       <h1>Gestor de Proyectos Académicos - ¡Te damos la bienvenida!</h1>
     </header>
   );
