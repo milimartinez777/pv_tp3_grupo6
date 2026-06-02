340f58974e5b0f5bd66d656187892961fb5cf763
Jonatan Cruz <anibalc279@gmail.com>
2026-06-02 20:45:57 -0300

    Creación del componente funcional RegistroActividad para primera parte del trabajo práctico, agrego archivo css correspondiente con estilos basicos de primera instancia.

---
+++ src/components/RegistroActividad.jsx
@@ -0,0 +1,11 @@
+import "../css/registroActividad.css";
+
+const RegistroActividad = ({ ultimaActualizacion }) => {
+  return (
+    <div className="registro-actividad">
+      <p>Última actualización de la lista: {ultimaActualizacion}</p>
+    </div>
+  );
+};
+
+export default RegistroActividad;
