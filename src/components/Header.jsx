0a89dacb77fd4eaf304518e05706af9d5163d10a
Jonatan Cruz <anibalc279@gmail.com>
2026-06-02 21:38:27 -0300

    Actualizo enlace y efecto sobre logo principal, correccion en nombre de llamada a archivo header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import "../css/header.css";
+import "../css/Header.css";
 
 import logo from "../assets/logito.png";
 
 const Header = () =>{
   return (
     <header className="encabezado-sitio">
+      <a href="/" className="enlace-logo">
       <img src={logo} alt="Logo del sitio" className="logo-sitio" />
+      </a>
       <h1>Gestor de Proyectos Académicos - ¡Te damos la bienvenida!</h1>
     </header>
   );
