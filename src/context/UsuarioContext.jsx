8ff0ebe44519d9b9af90979ee02383807ace100d
Jonatan Cruz <anibalc279@gmail.com>
2026-06-16 20:47:48 -0300

    inclui la funcion actualizarPerfil en el value del proveedor

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -3,16 +3,19 @@ import { createContext, useState } from "react";
 export const UsuarioContext = createContext();
 
 export const UsuarioProvider = ({ children }) => {
-  // Aquí definimos el estado inicial simulado del usuario
   const [usuario, setUsuario] = useState({
     nombre: "Juan Pérez",
     dni: "12345678",
     rol: "Alumno", // Puede ser "Docente" o "Alumno"
-    institucion: "Facultad de Informática"
+    institucion: "Facultad de Ingeniería"
   });
+  //Esta es la funcion para actualizar los datos del perfil
+  const actualizarPerfil = (nuevosDatos) => {
+    setUsuario(nuevosDatos);
+  };
 
   return (
-    <UsuarioContext.Provider value={{ usuario }}>
+    <UsuarioContext.Provider value={{ usuario, actualizarPerfil }}>
       {children}
     </UsuarioContext.Provider>
   );
